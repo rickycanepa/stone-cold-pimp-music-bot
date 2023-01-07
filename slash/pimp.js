93b7b4ed034964e445d5b7340d5d0fb1530334b2
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-01-07 16:39:45 -0600

    first commit

---
+++ slash/pimp.js
@@ -0,0 +1,5 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+const { QueryType } = require("discord-player")
+
+module.exports = {}
